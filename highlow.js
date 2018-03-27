ca3c32e30d7e204ebb069cffe19e97d33bcb545d
AJ <ajspeller@gmail.com>
2018-03-26 23:55:39 -0400

    Clear input and display each guess
    The input field is cleared after a guess.  The guess is also shown in the previous guesses panel.

--- highlow.js
+++ highlow.js
@@ -3,6 +3,24 @@ var newGame = () => {
   window.location.reload();
 };
 
+var logGuess = (guess) => {
+  document.querySelector("#guessLog").innerHTML += `${guess}, `;
+};
+
+var compareGuess = () => {
+  var userGuess = document.querySelector("input").value
+  console.log(userGuess);
+  if (userGuess > randomNumber) {
+    document.querySelector("#results").innerHTML = "Your guess was too high!";
+  } else if (userGuess < randomNumber) {
+    document.querySelector("#results").innerHTML = "Your guess was too low!";
+  } else {
+    document.querySelector("#results").innerHTML = "Your guess was correct!";
+  }
+
+  logGuess(userGuess);
+};
+
 document.body.onload = () => {
   randomNumber = Math.floor(Math.random() * 100 + 1);
   console.log(randomNumber);
@@ -10,4 +28,9 @@ document.body.onload = () => {
 
 document.querySelector("#newGame").addEventListener("click",function() {
   newGame();
+});
+
+document.querySelector("input").addEventListener("change",function(e){
+  compareGuess();
+  document.querySelector("input").value = "";
 });
